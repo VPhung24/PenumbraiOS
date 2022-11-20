f98b666c5b35e034389df0bddb24bb9a52454cb0
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2022-11-19 22:58:25 -0500

    test dep in PenumbraiOSProtos framework

---
+++ PenumbraiOSProtos/PenumbraiOSProtos.swift
@@ -0,0 +1,15 @@
+//
+//  PenumbraiOSProtos.swift
+//  PenumbraProtos
+//
+//  Created by Vivian Phung on 11/18/22.
+//
+
+import SwiftProtobuf
+
+public struct PenumbraiOSProtos {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
