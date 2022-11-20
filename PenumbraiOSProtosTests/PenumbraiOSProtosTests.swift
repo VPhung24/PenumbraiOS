7cdf80b28683c74fb8ca7978aa01a665b44aa700
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2022-11-19 23:44:06 -0500

    framework testing

---
+++ PenumbraiOSProtosTests/PenumbraiOSProtosTests.swift
@@ -0,0 +1,18 @@
+//
+//  PenumbraiOSProtosTests.swift
+//  PenumbraProtos
+//
+//  Created by Vivian Phung on 11/18/22.
+//
+
+import XCTest
+@testable import PenumbraiOSProtos
+
+final class PenumbraiOSProtosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PenumbraiOSProtos().text, "Hello, World!")
+    }
+}
